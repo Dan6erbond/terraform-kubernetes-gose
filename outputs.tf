a2d346e9cd1b3610926b9f57469bd25c1ca5c5ce
RaviAnand Mohabir <moravrav@gmail.com>
2023-01-27 16:13:03 +0100

    fix: :bug: fix reference to services in outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "service_name" {
   description = "Service name for Terraboard deployment"
-  value       = kubernetes_service.terraboard.metadata.0.name
+  value       = kubernetes_service.gose.metadata.0.name
 }
 
 output "service_port" {
   description = "Port exposed by the service"
-  value       = kubernetes_service.terraboard.spec.0.port.0.name
+  value       = kubernetes_service.gose.spec.0.port.0.name
 }
